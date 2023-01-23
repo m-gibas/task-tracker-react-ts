1fbcc59d22be0ab9e775a68f412d5a294134685a
Maksymilian Gibas <104219943+m-gibas@users.noreply.github.com>
2023-01-23 22:13:05 +0100

    final calculator functionality, routes update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import About from './components/About'
 import Tile from './calculator_components/Tile'
 import OperationTile from './calculator_components/OperationTile'
 
-
 export interface ArrTasksProps {
   id: number
   text: string
@@ -16,18 +15,27 @@ export interface ArrTasksProps {
   reminder: boolean
 }
 
+
+// Calculator Functionality
+
 export type ACTIONTYPE = 
   | { type: "add-digit"; payload: number | '.' }
   | { type: "delete-digit"; payload: undefined }
   | { type: "clear-all"; payload: undefined }
   | { type: "choose-operation"; payload: string }
-  | { type: "calculation"; payload: string };
+  | { type: "calculation"; payload: undefined };
 
-  // zmienic typ any
 function reducer(state: any, {type, payload}: ACTIONTYPE) {
   switch (type) {
     case "add-digit":
       if(state.currentInput) {
+        if(state.overwrite) {
+          return {
+            ...state,
+            currentInput: payload,
+            overwrite: false
+          }
+        }
         if(payload === 0 && state.currentInput === '0') return state 
         if(payload === '.' && state.currentInput.includes('.')) return state 
         if(payload !== '.' && payload !== 0 && state.currentInput.length === 1 && state.currentInput === '0') state.currentInput = ''
@@ -38,22 +46,40 @@ function reducer(state: any, {type, payload}: ACTIONTYPE) {
         // currentInput: `${state.currentInput || ''}${payload}`};
         currentInput: (state.currentInput || '') + payload };
     case "delete-digit":
+      if(state.currentInput == null) return state
+      if(state.overwrite) return { 
+        ...state,
+        overwrite: false,
+        currentInput: null
+      }
+      if(state.currentInput.length === 1) {
+        return {
+          ...state,
+          currentInput: null
+        }
+      }
       return { ...state,
-      currentInput: state.currentInput.slice(0, -1) };
+      currentInput: state.currentInput.slice(0, -1) 
+    };
     case "clear-all":
-      return { ...state,
-      currentInput: state.currentInput.slice(-1, 0) };
+      if(!state.currentInput && !state.previousInput) return state
+      return { };
     case "choose-operation":
       if(state.currentInput == null && state.previousInput == null) return state
+      if(state.currentInput == null ) {
+        return {
+          ...state,
+          operationSign: payload,
+        }
+      }
       if(state.previousInput == null) {
         return {
           ...state, 
           operationSign: payload,
-          previousInput: state.currentInput + payload,
+          previousInput: state.currentInput,
           currentInput: null
         }
       }
-
       return {
         ...state,
         previousInput: calculate(state),
@@ -61,8 +87,12 @@ function reducer(state: any, {type, payload}: ACTIONTYPE) {
         currentInput: null
       }
     case "calculation":
+      if(state.operationSign == null || state.currentInput == null || state.previousInput == null) return state
       return { ...state,
-      currentInput: state.currentInput };
+      previousInput: null,
+      operationSign: null,
+      overwrite: true,
+      currentInput: calculate(state) };
     default:
       throw new Error();
   }
@@ -76,9 +106,33 @@ function calculate({ currentInput, previousInput, operationSign }: any) {
   switch (operationSign) {
     case '+': 
       value = previous + current
+      break
+    case '-': 
+      value = previous - current
+      break
+    case '÷': 
+      value = previous / current
+      break
+    case '×': 
+      value = previous * current
+      break
   }
+  return value.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat('nb-NO', {
+  maximumFractionDigits: 0,
+} )
+
+function formatOperand(operand: any) {
+  if(operand == null) return
+  const [integer, decimal] = operand.split('.')
+  if(decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
+
+// Main
 function App() {
   const [showAddButton, setShowAddButton] = useState(false)
 
@@ -92,11 +146,9 @@ function App() {
       const dbTasks = await fetchTasks()
       setTasks(dbTasks)
     }
-
     getTasks()
   }, [])
 
-    // Fetch task from database
     const fetchTask = async (id: number) => {
       const res = await fetch(`http://localhost:5000/tasks/${id}`)
       const data: ArrTasksProps = await res.json()
@@ -104,7 +156,6 @@ function App() {
       return data
     }
 
-  // Fetch tasks from database
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
     const data: ArrTasksProps[] = await res.json()
@@ -112,7 +163,6 @@ function App() {
     return data
   }
 
-  // Add Task
   const addTask = async ( task: {text: string, day: string, reminder: boolean} )  => {
     const res = await fetch('http://localhost:5000/tasks', {
       method: 'POST', 
@@ -124,14 +174,12 @@ function App() {
     setTasks([...tasks, data])
   }
 
-  // Delete Task
   const deleteTask = async (id: number): Promise<void> => {
     await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
 
     setTasks(tasks.filter((task) => task.id !== id) )
   }
 
-  // Switch Reminder
   const switchReminder = async (id: number): Promise<void> => {
     const switchingTask = await fetchTask(id)
     const updatedTask = {...switchingTask, reminder: !switchingTask.reminder}
@@ -148,74 +196,55 @@ function App() {
   }
 
 
-  // Calculator functionality
-
-  
-
 
   return (
     <Router>
-      <div className="container">
-        <div className='calculator'>
-          <div className='output'>
-            <div className='previous-output'>{previousInput}</div>
-            <div className='current-output'>{currentInput}</div>
+      <Routes>
+        <Route path='/' element={
+          <div className="container">
+            <div className='calculator'>
+              <div className='output'>
+                <div className='previous-output'>{formatOperand(previousInput)} {operationSign}</div>
+                <div className='current-output'>{formatOperand(currentInput)}</div>
+              </div>
+              <button className='buttons double' onClick={() => dispatch({ type: 'clear-all', payload: undefined })} >C</button>
+              <button className='buttons double' onClick={() => dispatch({ type: 'delete-digit', payload: undefined })} >DEL</button>
+              <Tile value={7} dispatch={dispatch} />
+              <Tile value={8} dispatch={dispatch} />
+              <Tile value={9} dispatch={dispatch} />
+              <OperationTile value='÷' dispatch={dispatch} />
+              <Tile value={4} dispatch={dispatch} />
+              <Tile value={5} dispatch={dispatch} />
+              <Tile value={6} dispatch={dispatch} />
+              <OperationTile value='×' dispatch={dispatch} />
+              <Tile value={1} dispatch={dispatch} />
+              <Tile value={2} dispatch={dispatch} />
+              <Tile value={3} dispatch={dispatch} />
+              <OperationTile value='+' dispatch={dispatch} />
+              <Tile value={'.'} dispatch={dispatch} />
+              <Tile value={0} dispatch={dispatch} />
+              <button className='buttons' onClick={() => dispatch({ type: 'calculation', payload: undefined })} >=</button>
+              <OperationTile value='-' dispatch={dispatch} />
+            </div>
           </div>
-          {/* <Tile value='C' secondClass='double' />
-          <Tile value='DEL' secondClass='double' /> */}
-          <button className='buttons double' onClick={() => dispatch({ type: 'clear-all', payload: undefined })} >C</button>
-          <button className='buttons double' onClick={() => dispatch({ type: 'delete-digit', payload: undefined })} >DEL</button>
-          <Tile value={7} dispatch={dispatch} />
-          <Tile value={8} dispatch={dispatch} />
-          <Tile value={9} dispatch={dispatch} />
-          <OperationTile value='÷' dispatch={dispatch} />
-          <Tile value={4} dispatch={dispatch} />
-          <Tile value={5} dispatch={dispatch} />
-          <Tile value={6} dispatch={dispatch} />
-          <OperationTile value='×' dispatch={dispatch} />
-          <Tile value={1} dispatch={dispatch} />
-          <Tile value={2} dispatch={dispatch} />
-          <Tile value={3} dispatch={dispatch} />
-          <OperationTile value='+' dispatch={dispatch} />
-          <Tile value={'.'} dispatch={dispatch} />
-          <Tile value={0} dispatch={dispatch} />
-          <OperationTile value='=' dispatch={dispatch} />
-          <OperationTile value='-' dispatch={dispatch} />
+        } />
+    </Routes>
 
-          {/* <Tile value='7' onClick={addToInput} /> */}
-          {/* <Tile value='8' onClick={addToInput} />
-          <Tile value='9' onClick={addToInput} />
-          <Tile value='÷' onClick={addToInput} />
-          <Tile value='4' onClick={addToInput} />
-          <Tile value='5' onClick={addToInput} />
-          <Tile value='6' onClick={addToInput} />
-          <Tile value='×' onClick={addToInput} />
-          <Tile value='1' onClick={addToInput} />
-          <Tile value='2' onClick={addToInput} />
-          <Tile value='3' onClick={addToInput} />
-          <Tile value='+' onClick={addToInput} />
-          <Tile value='.' onClick={addToInput} />
-          <Tile value='0' onClick={addToInput} />
-          <Tile value='=' onClick={addToInput} />
-          <Tile value='-' onClick={addToInput} /> */}
-        </div>
-      </div>
-
-      <div className="container">
-        <Header title = "Task Tracker" onClick={() => setShowAddButton(!showAddButton)} showAddButton={showAddButton} />
-        <Routes>
-          <Route path='/' element={
-            <>
-              {showAddButton && <AddTask onClick={addTask} />}
-              { tasks.length !== 0 ? 
-              <Tasks tasks={tasks} onDelete={deleteTask} onDblClick={switchReminder} />
-              : "You have nothing to do!" }
-            </>
-          } />
-          <Route path='/about' element={ <About /> } />
-        </Routes>
-        <Footer />
-      </div>
+    <div className="container">
+      <Header title = "Task Tracker" onClick={() => setShowAddButton(!showAddButton)} showAddButton={showAddButton} />
+      <Routes>
+        <Route path='/' element={
+          <>
+            {showAddButton && <AddTask onClick={addTask} />}
+            { tasks.length !== 0 ? 
+            <Tasks tasks={tasks} onDelete={deleteTask} onDblClick={switchReminder} />
+            : "You have nothing to do!" }
+          </>
+        } />
+        <Route path='/about' element={ <About /> } />
+      </Routes>
+      <Footer />
+    </div>
       
     </Router>
   );
--- src/calculator_components/Tile/Tile.tsx
+++ src/calculator_components/Tile/Tile.tsx
@@ -3,16 +3,13 @@ import { ACTIONTYPE } from '../../App'
 
 export interface TileProps {
   value: number | '.'
-  // isDouble?: boolean
-  secondClass?: string
-  onClick?: (e: any) => void
   dispatch: React.Dispatch<ACTIONTYPE>
 }
 
-const Tile = ({ value, secondClass, onClick, dispatch }: TileProps) => {
+const Tile = ({ value, dispatch }: TileProps) => {
   return (
     // <button className={`buttons ${secondClass ? secondClass : ''}`} onClick={onClick} value={value}>{value}</button>
-    <button className={`buttons ${secondClass ? secondClass : ''}`} onClick={() => dispatch({ type: 'add-digit', payload: value })} value={value}>{value}</button>
+    <button className='buttons' onClick={() => dispatch({ type: 'add-digit', payload: value })} value={value}>{value}</button>
   )
 }
 
